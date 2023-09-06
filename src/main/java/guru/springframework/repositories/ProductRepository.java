750dcb4138e733bb30533a1113cd9ac678f0075b
boulanouar99mohamed <boulanouarmohamed99@gmail.com>
2023-09-07 00:09:12 +0100

    commit 1

--- src/main/java/guru/springframework/repositories/ProductRepository.java
+++ src/main/java/guru/springframework/repositories/ProductRepository.java
@@ -3,5 +3,5 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Long>{
+public interface ProductRepository extends CrudRepository<Product, Integer>{
 }
